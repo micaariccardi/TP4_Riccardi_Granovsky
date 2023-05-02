90545cd43dbd9d837b55c9597bd63190d3456369
MICAELA RICCARDI <47574710@ort>
2023-05-02 12:11:44 -0300

    Algunas Imagenes

--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -1,7 +1,7 @@
 static class Equipos
 {
-    public static List<string> listaEquipos {get; private set;} = {"Boca","River", "San Lorenzo", "Independiente", "Racing"}
-    public static List<string> listaMedias {get; private set;} //BUSCAR IMAGENES
+    public static List<string> listaEquipos {get; private set;} = {"Boca","River", "San Lorenzo", "Independiente", "Racing", "Saca chispas", "Huracán", "Argentina jr.", "Banfield", "Veléz"}
+    public static List<string> listaMedias {get; private set;} //BUSCAR 10 IMAGENES X CADA COSA
     public static List<string> listaPantalones {get; private set;}
     public static List<string> listaCamisetas {get; private set;}
     public static Dictionary <string, Indumentaria> EquiposIndumentaria {get; private set;}
