83623587d383b13bd986c28401333540f6ba6f83
MICAELA RICCARDI <47574710@ort>
2023-05-11 08:18:41 -0300

    terminamos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         ViewBag.IndumentariaEquipos = Equipos.IndumentariaEquipos;
-        return View(Index);
+        return View("Index");
     }
 
     public IActionResult SelectIndumentaria()
@@ -15,20 +15,26 @@ public class HomeController : Controller
         ViewBag.medias = Equipos.listaMedias;
         ViewBag.pantalones = Equipos.listaPantalones;
         ViewBag.camisetas = Equipos.listaCamisetas;
-        return View(SelectIndumentaria);
+        ViewBag.listaEquipos = Equipos.listaEquipos;
+        return View("SelectIndumentaria");
     }
     public IActionResult GuardarIndumentaria (int equipo=-1, int media=-1, int pantalon=-1, int remera=-1)
     {
         if(equipo == -1 || media == -1 || pantalon == -1 || remera == -1)
         {
             ViewBag.mensajeError="No seleccionaste todos los datos requeridos.";
-            return View(SelectIndumentaria);
+        ViewBag.medias = Equipos.listaMedias;
+        ViewBag.pantalones = Equipos.listaPantalones;
+        ViewBag.camisetas = Equipos.listaCamisetas;
+        ViewBag.listaEquipos = Equipos.listaEquipos;
+            return View("SelectIndumentaria");
         }
         else
         {
             Indumentaria x = new Indumentaria(Equipos.listaMedias[media], Equipos.listaPantalones[pantalon], Equipos.listaCamisetas[remera]);
             Equipos.IngresarIndumentaria(Equipos.listaEquipos[equipo], x);
+            ViewBag.IndumentariaEquipos = Equipos.IndumentariaEquipos;
         }
-        return View(Index);
+        return View("Index");
     }
 }
--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -1,10 +1,10 @@
 static class Equipos
 {
-    public static List<string> listaEquipos {get; private set;} = new List<string> {"Boca","River", "San Lorenzo", "Independiente", "Racing", "Sacachispas", "Huracán", "Argentina Jrs.", "Banfield", "Veléz"};
+    public static List<string> listaEquipos {get; private set;} = new List<string> {"Boca","River", "San Lorenzo", "Independiente", "Racing", "Defensa y Justicia", "Huracán", "Argentina Jrs.", "Banfield", "Veléz"};
     public static List<string> listaMedias {get; private set;} = new List<string> {"medias1.png","medias2.png","medias3.png","medias4.png","medias5.png","medias6.png","medias7.png","medias8.png","medias9.png","medias10.png"};
     public static List<string> listaPantalones {get; private set;} = new List<string> {"pantalon1.png", "pantalon2.png", "pantalon3.png", "pantalon4.png", "pantalon5.png", "pantalon6.png", "pantalon7.png", "pantalon8.png", "pantalon9.png", "pantalon10.png"};
     public static List<string> listaCamisetas {get; private set;} = new List<string> {"camiseta1.png","camiseta2.png","camiseta3.png","camiseta4.png","camiseta5.png","camiseta6.png","camiseta7.png","camiseta8.png","camiseta9.png","camiseta10.png"};
-    public static Dictionary <string, Indumentaria> IndumentariaEquipos {get; private set;}
+    public static Dictionary <string, Indumentaria> IndumentariaEquipos {get; private set;} = new Dictionary<string, Indumentaria>();
 
     public static bool IngresarIndumentaria(string equipoSeleccionado, Indumentaria item)
     {
